0ea98822ba421eed94a9e25f0a09178934668488
Carlos <carlos84593326@gmail.com>
2026-02-03 03:03:10 +0000

    public/firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,13 @@
-/* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
-const firebaseConfig = {
-  projectId: "studio-4803952210-fa8bf",
-  appId: "1:332002577508:web:55641406f4e36e7017a371",
-  apiKey: "AIzaSyApfPZJEp8ydYKMSXe6QDckwmzNYTDSFCc",
-  authDomain: "studio-4803952210-fa8bf.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "332002577508",
-  storageBucket: "studio-4803952210-fa8bf.appspot.com",
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    projectId: "hotel-rigut",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID"
+});
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
-  
-  const notificationTitle = payload.notification.title || 'Nueva Notificación';
-  const notificationOptions = {
-    body: payload.notification.body || '',
-    icon: '/favicon.ico',
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
